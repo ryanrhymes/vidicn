edec7a55d4564d560f883868080268d4602eb2b9
Liang Wang <ryanrhymes@gmail.com>
2013-01-13 22:54:58 +0200

    a simple solver

--- vidicn.py
+++ vidicn.py
@@ -20,7 +20,7 @@ from pulp import *
 # Model constants
 
 M = 10      # Number of routers
-N = 10000   # Number of files
+N = 100     # Number of files
 P = 100     # Number of chunks in a file
 
 
@@ -51,7 +51,7 @@ def prepare_content_distrib_var():
     return Y
 
 def prepare_decision_var():
-    X = zeros((N, P, M), dtype = int64)
+    X = [ "%i_%i_%i" % (i, j, k) for i in range(N) for j in range(P) for k in range(M) ]
     return X
 
 
@@ -75,19 +75,21 @@ class vidicn(object):
 
     def solve(self):
         # Create the 'prob' variable to contain the problem data
-        self.problem = LpProblem("The vidicn LP Problem", LpMinimize)
-        self.x_vars = LpVariable.dicts("x", self.x, 0.0, 100, LpContinuous)
+        self.problem = LpProblem("The vidicn LP Problem", LpMaximize)
+        self.x_vars = LpVariable.dicts('x', self.X, lowBound = 0, upBound = 1, cat = LpInteger)
 
         # Set objective, first function added to the prob
         print "Set objectives:", time.ctime()
         self.set_objective()
 
         # Set constraints
-        self.set_memory_constraints()
+        self.set_cache_constraints()
+        self.set_ncopy_constraints()
 
         # The problem data is written to an .lp file
         self.problem.writeLP("vidicn.lp")
         # The problem is solved using PuLP's choice of Solver
+        #self.problem.solve(COIN())
         self.problem.solve(GLPK())
 
         self.usedtime = time.time() - self.usedtime
@@ -96,47 +98,43 @@ class vidicn(object):
         pass
 
     def set_objective(self):
-        self.problem += 0.013 * self.x_vars['1'] + 0.008 * self.x_vars['2'], "Intra- and inter-ISP traffic"
+        objective = []
+        for i in range(N):
+            for j in range(P):
+                tmp1 = self.chunkSize[i,j] * self.chunkPopularity[i,j] * self.filePopularity[i]
+                for k in range(M):
+                    i_j_k = '%i_%i_%i' % (i, j, k)
+                    objective.append(tmp1 * (M - k) * self.x_vars[i_j_k])
+        self.problem += lpSum(objective), "Maximize byt hit rate and footprint reduction"
         pass
 
-    def set_memory_constraints(self):
-        self.problem += self.x_vars['1'] + self.x_vars['2'] == 100, "PercentagesSum"
-        self.problem += 0.100*self.x_vars['1'] + 0.200*self.x_vars['2'] >= 8.0, "ProteinRequirement"
-        self.problem += 0.080*self.x_vars['1'] + 0.100*self.x_vars['2'] >= 6.0, "FatRequirement"
-        self.problem += 0.001*self.x_vars['1'] + 0.005*self.x_vars['2'] <= 2.0, "FibreRequirement"
-        self.problem += 0.002*self.x_vars['1'] + 0.005*self.x_vars['2'] <= 0.4, "SaltRequirement"
-        pass
-
-    def set_natural_constraints(self):
-        for p in self.path.keys():
+    def set_cache_constraints(self):
+        for k in range(M):
             constraints = []
-            for r in self.path[p]:
-                p_r = '%i_%i' % (p, r)
-                constraints.append( str(self.d_vars[p_r]) )
-            self.lpfile.write('pnc_%i: %s <= 1.0\n' % (p, ' + '.join(constraints)))
-            #self.prob += lpSum(constraints) <= 1.0, ("Path %i natural constraints" % p)
+            for i in range(N):
+                for j in range(P):
+                    i_j_k = '%i_%i_%i' % (i, j, k)
+                    constraints.append(self.chunkSize[i,j] * self.x_vars[i_j_k])
+            self.problem += lpSum(constraints) <= self.cache[k], ("cache %i capacity constraint" % k)
         pass
 
-    def set_processing_constraints(self):
-        for r in self.L.keys():
-            constraints = []
-            for p in self.path.keys():
-                if r in self.path[p]:
-                    p_r = '%i_%i' % (p, r)
-                    constraints.append(self.d_vars[p_r] * self.v[p] / self.avgpkgsize)
-            for p, q in self.match.keys():
-                if r in self.path[p] and r in self.path[q]:
-                    q_r = '%i_%i' % (q, r)
-                    constraints.append(self.d_vars[q_r] * self.match[(p, q)])
-            self.prob += lpSum(constraints) <= self.L[r], ("Router %i processing constraints" % r)
+    def set_ncopy_constraints(self):
+        for i in range(N):
+            for j in range(P):
+                constraints = []
+                for k in range(M):
+                    i_j_k = '%i_%i_%i' % (i, j, k)
+                    constraints.append(self.x_vars[i_j_k])
+                self.problem += lpSum(constraints) <= 1.0, ("chunk %i_%i NCopy constraint" % (i,j))
         pass
 
     def output_result(self):
         # The status of the solution is printed to the screen
         print "Status:", LpStatus[self.problem.status]
         # Each of the variables is printed with it's resolved optimum value
+        f = open("vidicn.sol", "w")
         for v in self.problem.variables():
-            print "%s = %.2f" % (v.name, v.varValue)
+            f.write("%s = %.2f\n" % (v.name, v.varValue))
         pass
 
     pass
@@ -147,7 +145,7 @@ class vidicn(object):
 if __name__ == "__main__":
     obj = vidicn()
     obj.init_model()
-    #obj.solve()
-    #obj.output_result()
+    obj.solve()
+    obj.output_result()
 
     sys.exit(0)
