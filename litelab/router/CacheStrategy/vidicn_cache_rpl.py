08cbfeecf86637014aaf9058e9126320b0117676
Liang Wang <ryanrhymes@gmail.com>
2013-03-25 15:07:49 +0200

    new rpl

--- litelab/router/CacheStrategy/vidicn_cache_rpl.py
+++ litelab/router/CacheStrategy/vidicn_cache_rpl.py
@@ -21,8 +21,7 @@ class vidicn_cache_rpl(object):
         self.dtype = np.dtype([('fkey', np.int32), ('ckey', np.int32), ('utility', np.float64), \
                                    ('size', np.float64), ('timestamp', np.float64)])
         self.cache = np.array([], dtype=self.dtype)
-        self.llist = []
-        self.usedc = 0
+        self.usedc = 0.0
         self.quota = quota
         self.pathcache ={}
         self.logfh = None
@@ -32,54 +31,55 @@ class vidicn_cache_rpl(object):
     def keys(self):
         return self.llist
 
-    def add_chunk(self, key, size):
-        evict = (None, None)
+    def add_chunk(self, key, utility, size):
+        evict = []
         fkey, ckey = key
         ts = time.time()
-        utility = 0.0
         hit = np.where((self.cache['fkey']==fkey) & (self.cache['ckey']==ckey))[0]
         if len(hit):
             hit = hit[0]
+            self.cache[hit]['utility'] = utility
             self.cache[hit]['timestamp'] = ts
         else:
             if self.usedc + size > self.quota:
-                evcit_candidate = self.cache[0]
-                
+                t_usedc = self.usedc
+                t_utility = 0.0
+                t_evict = []
+                t_index = 0
+                while True:
+                    if (t_usedc + size <= self.quota) or (t_utility > utility):
+                        break
+                    ec = self.cache[t_index]
+                    t_usedc -= ec['size']
+                    t_utility += ec['utility']
+                    t_evict.append(t_index)
+                    t_index += 1
+                if (t_usedc + size <= self.quota) and (t_utility <= utility):
+                    self.usedc = t_usedc
+                    for eci in t_evict:
+                        evict.append(self.cache[eci])
+                    self.cache = np.delete(self.cache, t_evict, axis=0)
             else:
                 self.usedc += size
-                self.cache = np.append(self.cache, np.array([(fkey, ckey, utility, size, ts)], dtype=self.dtype), axis=0)
+            self.cache = np.append(self.cache, np.array([(fkey, ckey, utility, size, ts)], dtype=self.dtype), axis=0)
 
         self.cache = np.sort(self.cache, order=['utility', 'timestamp'])
         return evict
 
-    def add_chunk_old(self, key, size):
-        evict = (None, None)
-        if not self.cache.has_key(key) and self.quota > 0:
-            if len(self.llist) < self.quota:
-                # we still have spare space
-                self.cache[key] = val
-            else:
-                # well, run of space now
-                evkey = self.llist.pop()
-                edata = self.cache.pop(evkey)
-                evict = (evkey,edata)
-                self.cache[key] = val
-            self.llist.insert(0, key)
-        return evict
-
     def get_chunk(self, key):
-        val = self.cache.get(key, None)
-        if val:
-            self.llist.remove(key)
-            self.llist.insert(0, key)
-        return val
+        chunk = None
+        fkey, ckey = key
+        ts = time.time()
+        hit = np.where((self.cache['fkey']==fkey) & (self.cache['ckey']==ckey))[0]
+        if len(hit):
+            hit = hit[0]
+            chunk = self.cache[hit]
+            self.cache[hit]['timestamp'] = ts
+        self.cache = np.sort(self.cache, order=['utility', 'timestamp'])
+        return chunk
 
     def del_chunk(self, key):
-        evict = (None, None)
-        if self.cache.has_key(key):
-            self.llist.remove(key)
-            evict = (key,self.cache.pop(key))
-        return evict
+        pass
 
     def add_pathcache(self, key, src, dst):
         self.pathcache[key] = (src,dst)
@@ -112,7 +112,9 @@ def test_fn():
     random.seed(123)
     cache = vidicn_cache_rpl(50)
     for i in range(100):
-        cache.add_chunk((random.randint(0,5), random.randint(0,50)), 10*random.random())
+        evict = cache.add_chunk((random.randint(0,5), random.randint(0,50)), random.random(), 10*random.random())
+        if len(evict):
+            print "evict --->", evict
         #time.sleep(0.1)
 
     #print cache.cache
