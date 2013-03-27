9175bc4eb1e7e770df059aa58703f91458f20226
Liang Wang <ryanrhymes@gmail.com>
2013-03-27 18:39:59 +0200

    new exp

--- tree/script/calculate_dde_topx.sh
+++ tree/script/calculate_dde_topx.sh
@@ -3,19 +3,11 @@
 CSZ=$1
 CHK=$2
 CPY=10
-LOG="tree_xcopy_${CHK}chunk_csz${CSZ}.txt"
+LOG="tree_xcopy_${CHK}chunk_csz${CSZ}_topx.txt"
 
 APP1="/cs/fs/home/lxwang/cone/Papers/lxwang/vidicn/code/generate_request.py"
 APP2="/cs/fs/home/lxwang/cone/Papers/lxwang/vidicn/code/tree/treestatic_partial_relax.py"
-APP3="/cs/fs/home/lxwang/cone/Papers/lxwang/vidicn/code/tree/calculate_performance.py"
-
-$APP1 $CHK > trace_request.$CHK
-
-for x in `seq 1 $CPY`; do
-    $APP2 $CHK $x $CSZ
-done
-
-rm -rf $LOG
+APP3="/cs/fs/home/lxwang/cone/Papers/lxwang/vidicn/code/tree/script/calculate_dde_topx.py"
 
 for x in `seq 1 $CPY`; do
     $APP3 trace_request.$CHK tree_modelstatic_partial_relax.sol.$CHK.$x tree_modelstatic_partial_relax.chunk.$CHK.$x | tail -n 1 >> $LOG
