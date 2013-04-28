ddc626abe2f40d09d71f84694c1bb939b4c20baf
Liang Wang <ryanrhymes@gmail.com>
2013-04-28 15:51:26 +0300

    sequence generator

--- trace/sequence_generator.py
+++ trace/sequence_generator.py
@@ -1,58 +1,37 @@
 #!/usr/bin/env python
+"""
+Given a list of items with their corresponding frequency,
+sequence_generator generates a series of events consitent
+with the popularity distribution in the given list.
+
+This script is originally used for generating request series
+given the popularity distribution.
+
+Liang Wang @ Dept. of Computer Science, Univ. of Helsinki, Finland
+2013.04.28
+"""
 
 import os
 import sys
-import numpy as np
 import random
-from operator import itemgetter, attrgetter
+import operator
 
-AVGSIZE = 8.0
 
-def parsing(ifn):
-    print "parsing ..."
-    ifh = open(ifn, 'r')
-    obj = list()
-    total_length = 0
-    total_videos = 0
-    for line in ifh:
-        try:
-            fields = line.strip().split('|')
-            # Process time
-            tfs = fields[1].split(':')
-            tfv = 0
-            if len(tfs) == 3:
-                tfv += float(tfs[0]) * 3600 + float(tfs[1]) * 60 + float(tfs[2])
-            elif len(tfs) == 2:
-                tfv += float(tfs[0]) * 60 + float(tfs[1])
-            if tfv == 0:
-                tfv = random.randint(4, 12)
-            fields[1] = tfv
-            obj.append([fields[0], tfv, int(fields[2])])
-            total_length += tfv
-            total_videos += 1
-        except Exception, err:
-            pass
-    return obj, total_length / total_videos
-
-def format_trace(obj, avglength):
-    print "formatting trace ..."
-    obj = sorted(obj, key=itemgetter(2), reverse=True)
-    for i in range(len(obj)):
-        vo = obj[i]
-        vo[0] = "%07i" % i
-        vo[1] = vo[1] * AVGSIZE / avglength
-    return obj
-
-def write_trace(ofn, obj):
-    ofh = open(ofn, 'w')
-    for vo in obj:
-        ofh.write("%s %f %i\n" % (vo[0], vo[1], vo[2]))
-    ofh.close()
-    pass
-
+def sequence_generator(distr, count):
+    distr = sorted(distr, key=operator.itemgetter(1))
+    seqll = list()
+    # make intervals
+    for i in range(1, len(distr)):
+        distr[i][1] += distr[i-1][1]
+    # generate sequence
+    for i in range(count):
+        tmpi = random.randint(0, distr[-1][1])
+        y = filter(lambda x: x[1] - tmpi >= 0, distr)[0]
+        seqll.append(y)
+    return seqll
 
 if __name__ == "__main__":
-    obj, avglength = parsing(sys.argv[1])
-    obj = format_trace(obj, avglength)
-    write_trace("youtube.trace", obj)
+    x = [['a',5],['b',2],['c',3],['d',7],['e',1]]
+    seqll = sequence_generator(x, 20)
+    print seqll
     sys.exit(0)
